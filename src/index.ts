e6f6c694ed4bb4bf79ba4913bf74c4f432775356
sim1222 <kokt@sim1222.com>
2022-12-08 17:49:20 +0900

    add user-agent

--- src/index.ts
+++ src/index.ts
@@ -110,7 +110,11 @@ export default {
 		}
 
 		//Get Remote
-		const res = await fetch(reqUrl)
+		const res = await fetch(reqUrl, {
+			headers: {
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 simkey-MediaProxy https://github.com/sim1222/mediaprxy',
+			}
+		})
 		return await check(res);
 
 	},
